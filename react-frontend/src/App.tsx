ae622541f1a607be39617a10c65cbf730470272b
Moki <moking929@gmail.com>
2022-10-22 11:48:40 -0400

    react install

---
+++ react-frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit App.tsx in src folder, save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome to moki's game center
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

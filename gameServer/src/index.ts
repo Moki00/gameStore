dd2ae595dfe7fe27b98e9d46878ea0d6a91e8a53
Moki <moking929@gmail.com>
2022-10-24 09:18:58 -0400

    MV server in store

---
+++ gameServer/src/index.ts
@@ -0,0 +1,20 @@
+const express = require('express')
+
+import { connectToDatabase } from "./services/database.service"
+import { gamesRouter } from "./routes/games.router";
+
+const app = express();
+const port = 8080; // default port to listen
+
+connectToDatabase()
+    .then(() => {
+        app.use("/games", gamesRouter);
+
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
+    })
+    .catch((error: Error) => {
+        console.error("Database connection failed", error);
+        process.exit();
+    });
--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -101,6 +101,62 @@ export default App;
 
 /*
 
+// Imports
+import React from 'react';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+// import Home from './components/Home'
+const Home = lazy(() => import('./components/Home'));
+import { redirect } from "react-router-dom";
+
+const loader = async () => {
+  const user = await getUser();
+  if (!user) {
+    return redirect("/login");
+  }
+};
+
+
+// The App
+
+function App() {
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate("/home");
+  }
+
+// Export
+export default App;
+
+<BrowserRouter>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
+
+);
+}
+
+
+/* <div className="App">
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit App.tsx in src folder, save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Welcome to moki's game center
+  </a>
+</header>
+</div>
 
 
 import {

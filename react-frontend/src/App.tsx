c42e543115ef7a3c2e474daa5c2cbd4a84d2c741
Moki <moking929@gmail.com>
2022-10-24 02:16:13 -0400

    playing with react

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,30 +1,101 @@
 // Imports
-import React from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { ReactElement, ReactNode } from 'react';
 import './App.css';
-// import Home from './components/Home'
-const Home = lazy(() => import('./components/Home'));
-import { redirect } from "react-router-dom";
-
-const loader = async () => {
-  const user = await getUser();
-  if (!user) {
-    return redirect("/login");
-  }
-};
-
 
 // The App
 
+// Heading
+function Heading({ title }: { title: string }) {
+  return <h1>{title}</h1>;
+}
+
+function HeadingW({
+  children,
+}: {
+  children: ReactNode;
+}): ReactElement {
+  return <h1>{children}</h1>;
+}
+
+// default props
+const defaultCon = {
+  head: <strong>my heading</strong>,
+};
+type ConProps = { children: ReactNode } & typeof defaultCon;
+function Contain({ 
+  head,
+  children,
+ }: ConProps): ReactElement {
+  return (
+    <div>
+      <h1>{head}</h1>
+      {children}
+    </div>
+  );
+}
+
+Contain.defaultProps = defaultCon;
+
+// Functional props
+function TextWithNumber({
+  head,
+  children
+}: {
+  head?: (num: number) => ReactNode
+  children: (num: number) => ReactNode
+}) {
+  const [state, stateSet] = React.useState<number>(1);
+
+  return (
+    <div>
+      <h2>{head?.(state)}</h2>
+      <div>
+        {children(state)}
+      </div>
+      <div>
+        <button onClick={() => stateSet(state+1)}>Add</button>
+      </div>
+    </div>
+  )
+}
+
+// List
+function List<ListItem>({
+  items,
+  render,
+}: {
+  items: ListItem[],
+  render: (item: ListItem) => ReactNode
+}) {
+  return (
+    <ul>
+      {items.map((item, index)=>(
+        <li key={index}>{render(item)}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
-  let navigate = useNavigate();
-  function handleClick() {
-    navigate("/home");
+  return(
+    <div>
+      <Heading title="mokis games"></Heading>
+      <HeadingW>
+        <strong>hi</strong>
+      </HeadingW>
+      <Contain>foo</Contain>
+      <TextWithNumber head={(num: number) => <span>Header {num}</span>}>
+        {(num: number) => <div>add one: {num}</div> }
+      </TextWithNumber>
+      <List items={["BALLZ", "911", "gun"]} render={(item: string) => <div>{item.toLowerCase()}</div>}></List>
+    </div>
+  )
   }
 
 // Export
 export default App;
+
+
 
 // Trash
 
